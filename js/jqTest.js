cc943e45854125e9fe12f08c200a96fdb7f0afed
Chong Wang <wcssdy@gmail.com>
2017-08-18 23:39:34 +1200

    JQ ex

--- js/jqTest.js
+++ js/jqTest.js
@@ -14,6 +14,11 @@ $(document).ready(function(){
       if(toggleButton.text() == "Hide") toggleButton.text("Show");
       else toggleButton.text("Hide");
   });
+
+  var listItems = $("header nav li");
+  listItems.css("font-weight", "bold");
+  listItems.filter(":first").css("font-size","18px"); //similar as $("header nav li:first")
+
 })
 
 
